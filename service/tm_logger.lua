95431fb80650b55d8b6f178fc861cd6c88e3f3a6
Hanlei Qin <qinhanlei@gmail.com>
2016-10-15 18:47:30 +0800

    update logger.

--- service/tm_logger.lua
+++ service/tm_logger.lua
@@ -1,21 +1,56 @@
 local skynet = require "skynet"
 require "skynet.manager"
+local queue = require "skynet.queue"
+local cs = queue()
+
 local nodename = skynet.getenv("nodename")
 
+local MB = 1024 * 1024
+local GB = 1024 * MB
+local FILE_LIMIT = 2 * MB
+
 local CMD = {}
+local logfile = nil
+local filesize = 0
+local fileidx = 0
 
 
-local function str_now()
-	local t = os.date("*t")
-	return string.format("%04d-%02d-%02d %02d:%02d:%02d", 
-        t.year, t.month, t.day, t.hour, t.min, t.sec)
+local function str_datetime(t, p)
+	t = t or os.date("*t")
+	p = p or math.floor(skynet.time()*100%100)
+	return string.format("%04d-%02d-%02d %02d:%02d:%02d.%02d", 
+        t.year, t.month, t.day, t.hour, t.min, t.sec, p)
+end
+
+
+local function logging(source, typ, log)
+	cs(function()
+		local t = os.date("*t")
+		local p = math.floor(skynet.time()*100%100)
+		local tm = str_datetime(t,p)
+		local log = string.format("[%s] [%s] [%s:%x] %s", tm, typ, nodename, source, log)
+		print(log)
+		
+		if not logfile then
+			local logpath = string.format("./logs/%s_%04d%02d%02d_%02d%02d%02d_%02d.log",
+					nodename, t.year, t.month, t.day, t.hour, t.min, t.sec, fileidx)
+			logfile = io.open(logpath, "a+")
+		end
+		logfile:write(log .. "\n")
+	    logfile:flush()
+		filesize = filesize + string.len(log) + 1
+		if logfile and filesize >= FILE_LIMIT then
+			logfile:close()
+			logfile = nil
+			filesize = 0
+			fileidx = fileidx + 1
+		end
+	end)
 end
 
 
 function CMD.logging(source, typ, log)
-    local log = string.format("[%s] [%s] [%s:%x] %s", str_now(), typ, nodename, source, log)
-    print(log)
-    --TODO: write to file.
+    logging(source, typ, log)
 end
 
 
@@ -24,7 +59,7 @@ skynet.register_protocol {
 	id = skynet.PTYPE_TEXT,
 	unpack = skynet.tostring,
 	dispatch = function(_, address, msg)
-        CMD.logging(address, "SKY", msg)
+        logging(address, "SKY", msg)
 	end
 }
 
@@ -36,4 +71,5 @@ skynet.start(function()
 	end)
 
 	skynet.register(".logger")
+	logging(skynet.self(), "INFO", "terminator-logger ready.")
 end)
--- test/testlogger.lua
+++ test/testlogger.lua
@@ -5,4 +5,12 @@ local logger = require "simple-logger"
 skynet.start(function()
     tlog.debug("this is test of logger.")
     
+    local n = 100000
+    local begin_time = skynet.now()
+    for i = 1, n do
+        tlog.debug("here is logging the log:" .. i)
+    end
+    local end_time = skynet.now()
+    tlog.debug("logging %d logs, cost time:%fs", n, (end_time - begin_time)/100.0)
+    
 end)
