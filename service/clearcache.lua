c3fcb7537e6dd2e33fb8d93e6296dab12371f807
Hanlei Qin <qinhanlei@gmail.com>
2016-10-26 22:01:17 +0800

    fixed.

--- service/clearcache.lua
+++ service/clearcache.lua
@@ -1,11 +1,10 @@
 local skynet = require "skynet"
 local codecache = require "skynet.codecache"
 
-local logger = require "simple-logger"
-
-
 skynet.start(function()
-    codecache.clear()
     tlog.info("clear codecache ...")
+    
+    codecache.clear()
+    
     skynet.exit()
 end)
