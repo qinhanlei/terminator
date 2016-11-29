5e427e16541e5dd1e926af721c97ed4cb7bec3d1
Hanlei Qin <qinhanlei@gmail.com>
2016-11-29 22:47:59 +0800

    add util timer.

--- lualib/util.lua
+++ lualib/util.lua
@@ -1,3 +1,5 @@
+local skynet = require "skynet"
+
 local util = {}
 
 
@@ -20,6 +22,35 @@ function util.nowstr()
 	local t = os.date("*t")
 	return string.format("%04d-%02d-%02d %02d:%02d:%02d", 
 			t.year, t.month, t.day, t.hour, t.min, t.sec)
+end
+
+
+function util.newtimer()
+    local handles = {}
+    
+    function timer.timeout(ti, f)
+    	local function tf()
+    		local f = handles[tf]
+    		if f then
+    			f()
+    		end
+    	end
+    	skynet.timeout(ti, tf)
+        handles[tf] = f
+    	return tf
+    end
+    
+    function timer.cancel(tf)
+    	handles[tf] = nil
+    end
+    
+    function timer.clear()
+    	for k, _ in pairs(handles) do
+    		handles[k] = nil
+    	end
+    end
+    
+    return timer
 end
 
 
