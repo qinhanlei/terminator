8cf497cf0ebb7fe3d369319601f725ae1d61275a
Hanlei Qin <qinhanlei@gmail.com>
2017-05-26 10:28:15 +0800

    update charset for wechat emoji like.

--- service/tm_mysql.lua
+++ service/tm_mysql.lua
@@ -28,7 +28,7 @@ local function connect(dbname, t)
 			password = t.pass or _mconf.pass,
 			max_packet_size = 1024 * 1024,
 			on_connect = function(c)
-				c:query("set charset utf8")
+				c:query("set charset utf8mb4")
 			end
 		})
 		if c then
@@ -79,7 +79,7 @@ local function keep_alive()
 			for db, t in pairs(_conn) do
 				for i, c in pairs(t) do
 					tlog.debug("keep alive %s:%d connection...", db, i)
-					c:query("set charset utf8")  --TODO: verify this
+					c:query("set charset utf8mb4")  --TODO: verify this
 					skynet.sleep(0.5*100)
 				end
 			end
