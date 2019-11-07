11342c627f2f3139ba892ab2034f3aadaf670874
Hanlei Qin <qinhanlei@gmail.com>
2019-11-08 00:11:15 +0800

    up

--- theone/client.lua
+++ theone/client.lua
@@ -2,21 +2,27 @@ local skynet = require "skynet"
 local websocket = require "http.websocket"
 local log = require "log"
 
+-- require "protobuf"
+local parser = require "parser"
+parser.register("msg.proto", "./theone")
+
+
 local clientid = ...
 
-function start()
+local function start()
 	local ws_id = websocket.connect("ws://127.0.0.1:10086")
 	local count = 1
-	log.debug("this is echo client:"..clientid)
+	clientid = clientid or 0
+	log.debug("this is echo client%d", clientid)
 	while true do
 		local msg = "hello world! " .. count
 		count = count + 1
 		websocket.write(ws_id, msg)
-		log.debug(clientid.." >>>: " .. msg)
+		log.debug("client%d >>> %s", clientid, msg)
 		local resp, close_reason = websocket.read(ws_id)
-		log.debug(clientid.." <<<: " .. (resp and resp or "[Close] " .. close_reason))
+		log.debug("client%d <<< %s", clientid, (resp and resp or "[Close] " .. close_reason))
 		if not resp then
-			log.debug(clientid .. "echo server close.")
+			log.debug("client%d echo server close", clientid)
 			break
 		end
 		websocket.ping(ws_id)
